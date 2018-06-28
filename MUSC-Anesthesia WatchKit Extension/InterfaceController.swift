8c5dd7b8ef47098737ab989fba656078b89a97e2
nick3tt <nthreat@g.clemson.edu>
2018-06-28 12:50:55 -0700

    June 28th, 2018 -- Reading Data from TextFile and storing it into an array

--- MUSC-Anesthesia WatchKit Extension/InterfaceController.swift
+++ MUSC-Anesthesia WatchKit Extension/InterfaceController.swift
@@ -14,29 +14,19 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var MedicalIssue: WKInterfaceLabel!
     @IBOutlet var Data: WKInterfaceLabel!
     
+    var txtData: [String]?
+    var patientNameRoom: String?
+    var patientIssue: String?
+    var patientData: String?
+    
+    let fileName: String = "patients"
 
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
         super.awake(withContext: context)
-
-
-            NameRoom.setTextColor(UIColor.magenta)
-            NameRoom.setText("Smith - OR 1")
-            
-            MedicalIssue.setTextColor(UIColor.magenta)
-            MedicalIssue.setText("NBP")
-            
-            Data.setTextColor(UIColor.magenta)
-            Data.setText("100/60")
         
-            NameRoom.setTextColor(UIColor.cyan)
-            NameRoom.setText("Smith - OR 1")
-            
-            MedicalIssue.setTextColor(UIColor.cyan)
-            MedicalIssue.setText("SpO2")
-            
-            Data.setTextColor(UIColor.cyan)
-            Data.setText("91%")
+        txtData = readTXTIntoArray(file: fileName)
+        displayBP()
         
     }
     
@@ -48,5 +38,42 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    func readTXTIntoArray(file: String) -> [String]? {
+        guard let path = Bundle.main.path(forResource: fileName, ofType: "txt") else {
+            return nil
+        }
+            
+        do {
+            let content = try String(contentsOfFile:path, encoding: String.Encoding.utf8)
+            print(content.components(separatedBy: ["\n", ",", "\t", "\r"]))
+            return content.components(separatedBy: " ")
+        } catch {
+            return nil
+        }
+        
+    }
+    
+    func displayBP() {
+        NameRoom.setTextColor(UIColor.magenta)
+        NameRoom.setText("Smith - OR 1")
+        
+        MedicalIssue.setTextColor(UIColor.magenta)
+        MedicalIssue.setText("NBP")
+        
+        Data.setTextColor(UIColor.magenta)
+        Data.setText("100/60")
+    }
+    
+    func displaySP02() {
+        NameRoom.setTextColor(UIColor.cyan)
+        NameRoom.setText("Smith - OR 1")
+        
+        MedicalIssue.setTextColor(UIColor.cyan)
+        MedicalIssue.setText("SpO2")
+        
+        Data.setTextColor(UIColor.cyan)
+        Data.setText("91%")
     }
 }
