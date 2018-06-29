2ccb6274a20435ab1bc2efa8770efe187683a06e
nick3tt <nthreat@g.clemson.edu>
2018-06-29 11:40:21 -0700

    June 29th, 2018 -- Changing scenes

--- MUSC-Anesthesia WatchKit Extension/InterfaceController.swift
+++ MUSC-Anesthesia WatchKit Extension/InterfaceController.swift
@@ -15,19 +15,36 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var Data: WKInterfaceLabel!
     
     var txtData: [String]?
-    var patientNameRoom: String?
-    var patientIssue: String?
-    var patientData: String?
+    var patientNameRoom = [String]()
+    var patientIssue = [String]()
+    var patientData = [String]()
+    
+    var rows = Int()
+    var i = 0
     
     let fileName: String = "patients"
-
+    
+    let nameCol = 0
+    let roomCol = 1
+    let issueCol = 2
+    let dataCol = 3
+    let numCols = 4
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
         super.awake(withContext: context)
         
         txtData = readTXTIntoArray(file: fileName)
-        displayBP()
+        assignLables()
         
+        _ = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(enableDisplay), userInfo: nil, repeats: true)
+//        displayBP()
+//        i += 1
+//        sleep(5)
+        
+//        displaySP02()
+//        i += 1
+//        sleep(5)
     }
     
     override func willActivate() {
@@ -47,33 +64,78 @@ class InterfaceController: WKInterfaceController {
             
         do {
             let content = try String(contentsOfFile:path, encoding: String.Encoding.utf8)
-            print(content.components(separatedBy: ["\n", ",", "\t", "\r"]))
-            return content.components(separatedBy: " ")
+            
+            // Filter array
+            var contentsFiltered = content.components(separatedBy: ["\n", ",", "\t", "\r"])
+            
+            contentsFiltered = contentsFiltered.filter({$0 != ""})
+            
+            for _ in 0..<numCols  {
+                contentsFiltered.removeFirst()
+            }
+            
+            rows = contentsFiltered.count / numCols
+            
+            return contentsFiltered
         } catch {
             return nil
         }
-        
     }
     
+    func assignLables() {
+        var index = 0
+        while(index < (txtData?.count)!){
+            patientNameRoom.append((txtData?[index + nameCol])! + " - " + (txtData?[index + roomCol])!)
+            patientIssue.append((txtData?[index + issueCol])!)
+            patientData.append((txtData?[index + dataCol])!)
+            
+            index += numCols
+        }
+    }
+    
+    @objc func enableDisplay() {
+        switch(patientIssue[i]) {
+        case "NBP":
+            displayBP()
+        case "SpO2":
+            displaySP02()
+        default:
+            print(patientIssue[i])
+        }
+        
+        i += 1
+        checkIterator()
+    }
+   
     func displayBP() {
         NameRoom.setTextColor(UIColor.magenta)
-        NameRoom.setText("Smith - OR 1")
+        NameRoom.setText(patientNameRoom[i])
         
         MedicalIssue.setTextColor(UIColor.magenta)
-        MedicalIssue.setText("NBP")
+        MedicalIssue.setText(patientIssue[i])
         
         Data.setTextColor(UIColor.magenta)
-        Data.setText("100/60")
+        Data.setText(patientData[i])
+    
+//        i += 1
     }
     
     func displaySP02() {
         NameRoom.setTextColor(UIColor.cyan)
-        NameRoom.setText("Smith - OR 1")
+        NameRoom.setText(patientNameRoom[i])
         
         MedicalIssue.setTextColor(UIColor.cyan)
-        MedicalIssue.setText("SpO2")
+        MedicalIssue.setText(patientIssue[i])
         
         Data.setTextColor(UIColor.cyan)
-        Data.setText("91%")
+        Data.setText(patientData[i])
+        
+//        i += 1
+    }
+    
+    func checkIterator() {
+        if(i >= rows) {
+            i = 0
+        }
     }
 }
