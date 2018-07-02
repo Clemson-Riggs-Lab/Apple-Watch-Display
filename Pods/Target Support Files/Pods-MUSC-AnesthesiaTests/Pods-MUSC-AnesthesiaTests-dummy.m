b91cfa72bfb46eff7962fbacf27bba8d1db4a406
Kylie Gomes <KylieHawaii@9.237.21.198.tigernet.wifi.dyn.clemson.edu>
2018-07-02 15:57:46 -0400

    July 2ns, 2018 -- 4:00 PM

--- Pods/Target Support Files/Pods-MUSC-AnesthesiaTests/Pods-MUSC-AnesthesiaTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MUSC_AnesthesiaTests : NSObject
-@end
-@implementation PodsDummy_Pods_MUSC_AnesthesiaTests
-@end
--- Pods/Target Support Files/Pods-MUSC-AnesthesiaUITests/Pods-MUSC-AnesthesiaUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MUSC_AnesthesiaUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MUSC_AnesthesiaUITests
-@end
