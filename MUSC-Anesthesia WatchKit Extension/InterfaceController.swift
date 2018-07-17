66f5695bf3f2b3764b95d6a801fe18759a322a69
nick3tt <nthreat@g.clemson.edu>
2018-07-17 14:52:14 -0400

    July 17th, 2018 -- Clean Code Part 2

--- MUSC-Anesthesia WatchKit Extension/InterfaceController.swift
+++ MUSC-Anesthesia WatchKit Extension/InterfaceController.swift
@@ -17,7 +17,11 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var Data: WKInterfaceLabel!
     @IBOutlet var Et: WKInterfaceLabel!
     @IBOutlet var DirectionArrow: WKInterfaceLabel!
-    @IBOutlet var underline: WKInterfaceSeparator!
+    @IBOutlet var Underline: WKInterfaceSeparator!
+    
+    // File Info
+    let fileName: String = "patients"
+    let numCols = 5
     
     // Variables used to store data from Txt/CSV file
     var txtData: [String]?
@@ -25,23 +29,13 @@ class InterfaceController: WKInterfaceController {
     var patientIssue = [String]()
     var patientData = [String]()
     var arrowTxt = [String]()
-    var NBPData = [String]()
-    var SpO2Data = [String]()
-    var CO2Data = [String]()
 
-    
     // Variables used to temporarily hold data that should be compared
     var currentPatientIssue = String()
-    var currentData = String()
-//    var prevData = String()
 
     // Iterate through data
     var rows = Int()
     var i = 0
-    
-    // File Info
-    let fileName: String = "patients"
-    let numCols = 5
     
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
@@ -51,7 +45,7 @@ class InterfaceController: WKInterfaceController {
         formatDisplay()
         setFontToBold()
         
-        // Read txt file
+        // Read txt file and store its data
         txtData = readTXTIntoArray(file: fileName)
         
         // Assign Labels proper data
@@ -103,7 +97,7 @@ class InterfaceController: WKInterfaceController {
         let arrowCol = 4
         
         var index = 0
-        while(index < (txtData?.count)!){
+        while(index < (txtData?.count)!) {
             patientNameRoom.append((txtData?[index + nameCol])! + " - " + (txtData?[index + roomCol])!)
             patientIssue.append((txtData?[index + issueCol])!)
             patientData.append((txtData?[index + dataCol])!)
@@ -116,7 +110,7 @@ class InterfaceController: WKInterfaceController {
     func formatDisplay() {
         // Black out display labels
         NameRoom.setTextColor(UIColor.black)
-        underline.setColor(UIColor.black)
+        Underline.setColor(UIColor.black)
         MedicalIssue.setTextColor(UIColor.black)
         Data.setTextColor(UIColor.black)
         Et.setTextColor(UIColor.black)
@@ -134,158 +128,65 @@ class InterfaceController: WKInterfaceController {
         MedicalIssue.setAttributedText(bold)
         Et.setAttributedText(bold)
         Data.setAttributedText(bold)
+        DirectionArrow.setAttributedText(bold)
     }
     
     @objc func enableDisplay() {
-        // Variable to store data strings as integers
-//        var currentDataInt = Int()
-        
-//        Et.setTextColor(UIColor.black)
-//        DirectionArrow.setTextColor(UIColor.black)
-        
+        var NBPData = [String]()
+        var SpO2Data = [String]()
+        var CO2Data = [String]()
+//        var currentData = String()
+        var color = UIColor()
+//        currentData = patientData[i]
         currentPatientIssue = patientIssue[i]
-        currentData = patientData[i]
-        
         switch(currentPatientIssue) {
         case "NBP":
             NBPData.append(patientData[i])
-            displayBP()
+            color = UIColor.magenta
         case "SpO2":
-            SpO2Data.append(patientData[i])
-        
             // Get rid of % sign
-            currentData = currentData.replacingOccurrences(of: "%", with: "")
-//            currentDataInt = Int(currentData)!
-            
-            displaySP02()
+//            currentData = currentData.replacingOccurrences(of: "%", with: "")
+            SpO2Data.append(patientData[i])
+            color = UIColor.cyan
         case "CO2":
             CO2Data.append(patientData[i])
-//            currentDataInt = Int(currentData)!
-            displayCO2()
+            color = UIColor.white
         default:
             print(patientIssue[i])
         }
+        displayInterface(interfaceColor: color)
         
         i += 1
         checkIterator()
     }
     
-    func displayBP() {
-        NameRoom.setTextColor(UIColor.magenta)
+    func displayInterface(interfaceColor: UIColor) {
+        NameRoom.setTextColor(interfaceColor)
         NameRoom.setText(patientNameRoom[i])
         
-        underline.setColor(UIColor.magenta)
+        Underline.setColor(interfaceColor)
         
-        MedicalIssue.setTextColor(UIColor.magenta)
-        MedicalIssue.setText(patientIssue[i])
+        MedicalIssue.setTextColor(interfaceColor)
+        if(currentPatientIssue == "CO2") {
+            MedicalIssue.setText("CO2 mm Hg")
+            
+            Et.setTextColor(interfaceColor)
+            Et.setText("Et")
+        }
+        else {
+            MedicalIssue.setText(patientIssue[i])
+            Et.setTextColor(UIColor.black)
+        }
         
-        Data.setTextColor(UIColor.magenta)
+        Data.setTextColor(interfaceColor)
         Data.setText(patientData[i])
+        
+        DirectionArrow.setTextColor(interfaceColor)
+        DirectionArrow.setText(String(UnicodeScalar(Int(arrowTxt[i], radix: 16)!)!))
+        
+        vibrationEffect()
+    }
 
-        DirectionArrow.setTextColor(UIColor.magenta)
-        DirectionArrow.setText(String(UnicodeScalar(Int(arrowTxt[i], radix: 16)!)!))
-        vibrationEffect()
-    }
-    
-    func displaySP02() {
-        NameRoom.setTextColor(UIColor.cyan)
-        NameRoom.setText(patientNameRoom[i])
-        
-        underline.setColor(UIColor.cyan)
-        
-        MedicalIssue.setTextColor(UIColor.cyan)
-        MedicalIssue.setText(patientIssue[i])
-        
-        Data.setTextColor(UIColor.cyan)
-        Data.setText(patientData[i])
-        
-        DirectionArrow.setTextColor(UIColor.cyan)
-        DirectionArrow.setText(String(UnicodeScalar(Int(arrowTxt[i], radix: 16)!)!))
-        vibrationEffect()
-        
-/*      let standardSpO2 = 95
-        let smalldrop = 5
-        let slightdrop = 10
-        let majordrop = 15
-         
-        if(standardSpO2 > currentDataInt) {
-            if(standardSpO2 - currentDataInt >= majordrop) {
-                // 45 Double Down Arrow
-                DirectionArrow.setText("\u{21CA}")
-                WKInterfaceDevice.current().play(.notification)
-            }
-            else if(standardSpO2 - currentDataInt >= slightdrop) {
-                // 45 Single Down Arow
-                DirectionArrow.setText("\u{2198}")
-                WKInterfaceDevice.current().play(.stop)
-            }
-            else if(standardSpO2 - currentDataInt >= smalldrop) {
-                // Vertical Down Arrow
-                DirectionArrow.setText("\u{2193}")
-                WKInterfaceDevice.current().play(.directionDown)
-            }
-        }
-        else if(standardSpO2 < currentDataInt) {
-            // Vertical Up Arrow
-            DirectionArrow.setText("\u{2191}")
-        }
-        else {
-            DirectionArrow.setText(" ")
-        }
-*/
-    }
-    
-    func displayCO2() {
-        NameRoom.setTextColor(UIColor.white)
-        NameRoom.setText(patientNameRoom[i])
-        
-        underline.setColor(UIColor.white)
-        
-        MedicalIssue.setTextColor(UIColor.white)
-        MedicalIssue.setText("CO2 mm Hg")
-        
-        Et.setTextColor(UIColor.white)
-        Et.setText("Et")
-        
-        Data.setTextColor(UIColor.white)
-        Data.setText(patientData[i])
-        
-        DirectionArrow.setTextColor(UIColor.white)
-        DirectionArrow.setText(String(UnicodeScalar(Int(arrowTxt[i], radix: 16)!)!))
-        vibrationEffect()
-/*
-        let standardEtCO2 = 35
-        let smalldrop = 5
-        let slightdrop = 10
-        let majordrop = 15
-         
-        if(standardEtCO2 > currentDataInt) {
-            if(standardEtCO2 - currentDataInt >= majordrop) {
-                // 45 Double Down Arrow
-                DirectionArrow.setText("\u{21CA}")
-                WKInterfaceDevice.current().play(.notification)
-            }
-            else if(standardEtCO2 - currentDataInt >= slightdrop) {
-                // 45 Single Down Arow
-                DirectionArrow.setText("\u{2198}")
-                WKInterfaceDevice.current().play(.stop)
-            }
-            else if(standardEtCO2 - currentDataInt >= smalldrop) {
-                // Vertical Down Arrow
-                DirectionArrow.setText("\u{2193}")
-                WKInterfaceDevice.current().play(.directionDown)
-            }
-        }
-        else if(standardEtCO2 < currentDataInt) {
-            // Vertical Up Arrow
-            DirectionArrow.setText("\u{2191}")
-        }
-        else {
-            DirectionArrow.setText(" ")
-        }
- */
-    }
-    
     func vibrationEffect() {
         switch(arrowTxt[i]) {
         case "2193":
